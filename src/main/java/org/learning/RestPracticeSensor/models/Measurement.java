158f44e7e53a9ae19851459b361bf689b9213e08
Artem <maslijartei@gmail.com>
2023-08-31 17:01:07 +0300

    Add measurement as entity in DB, ManyToOne relationship with sensor

---
+++ src/main/java/org/learning/RestPracticeSensor/models/Measurement.java
@@ -0,0 +1,80 @@
+package org.learning.RestPracticeSensor.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Measurement")
+public class Measurement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "value")
+    @NotNull
+    @Min(-100)
+    @Max(100)
+    private Double value;
+
+    @Column(name = "raining")
+    @NotNull
+    private Boolean raining;
+
+    @NotNull
+    @Column(name = "measurement_date_time")
+    private LocalDateTime measurementDateTime;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "sensor", referencedColumnName = "name")
+    private Sensor sensor;
+
+    public Measurement() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Boolean isRaining() {
+        return raining;
+    }
+
+    public void setRaining(Boolean raining) {
+        this.raining = raining;
+    }
+
+    public LocalDateTime getMeasurementDateTime() {
+        return measurementDateTime;
+    }
+
+    public void setMeasurementDateTime(LocalDateTime measurementDateTime) {
+        this.measurementDateTime = measurementDateTime;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+}
