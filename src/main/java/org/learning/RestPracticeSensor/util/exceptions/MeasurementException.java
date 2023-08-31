7ddf0b1b410eb0f1812eb4ef3d01985492fae65b
Artem <maslijartei@gmail.com>
2023-08-31 17:08:43 +0300

    Add measurement custom exception

---
+++ src/main/java/org/learning/RestPracticeSensor/util/exceptions/MeasurementException.java
@@ -0,0 +1,8 @@
+package org.learning.RestPracticeSensor.util.exceptions;
+
+public class MeasurementException extends RuntimeException{
+
+    public MeasurementException(String msg) {
+        super(msg);
+    }
+}
