3243efb787fee71aa8ae154e1fe5e8cd5a4c8476
Artem <maslijartei@gmail.com>
2023-08-31 17:17:49 +0300

    Application is completed, penultimate commit

---
+++ src/main/java/org/learning/RestPracticeSensor/RestPracticeSensorApplication.java
@@ -0,0 +1,13 @@
+package org.learning.RestPracticeSensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPracticeSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPracticeSensorApplication.class, args);
+	}
+
+}
