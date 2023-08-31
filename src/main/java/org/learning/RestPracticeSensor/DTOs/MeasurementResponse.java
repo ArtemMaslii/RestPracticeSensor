ea48320091dbac05eae32628e0dd52b89224aab5
Artem <maslijartei@gmail.com>
2023-08-31 17:02:57 +0300

    Add measurementResponse class to beatify json output

---
+++ src/main/java/org/learning/RestPracticeSensor/DTOs/MeasurementResponse.java
@@ -0,0 +1,21 @@
+package org.learning.RestPracticeSensor.DTOs;
+
+import org.learning.RestPracticeSensor.DTOs.MeasurementDTO;
+
+import java.util.List;
+
+public class MeasurementResponse {
+    private List<MeasurementDTO> measurements;
+
+    public MeasurementResponse(List<MeasurementDTO> measurements) {
+        this.measurements = measurements;
+    }
+
+    public List<MeasurementDTO> getMeasurements() {
+        return measurements;
+    }
+
+    public void setMeasurements(List<MeasurementDTO> measurements) {
+        this.measurements = measurements;
+    }
+}
